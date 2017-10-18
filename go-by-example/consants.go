c0cb723a2c027e5c8fa4f0a11e592dc797e7a7fa
Jonathan Ankiewicz <jankiewicz@ten-x.com>
2017-10-17 21:55:14 -0700

    updating some golang practices

---
+++ go-by-example/consants.go
@@ -0,0 +1,21 @@
+
+package main
+
+import "fmt"
+import "math"
+
+const s string = "constant"
+
+func main() {
+  fmt.Println(s)
+
+  const n = 500000
+
+  const d = 3e20 / n
+  fmt.Println(s)
+
+  fmt.Println(int64(d))
+
+  fmt.Println(math.Sin(n))
+
+}
